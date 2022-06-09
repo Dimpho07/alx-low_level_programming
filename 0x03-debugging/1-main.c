0c8155e8300e9f6209fb5bb892c0959f7cd8a867
Dimpho Aphiri <dimplesaphiri@gmail.com>
2022-06-09 05:56:38 -0700

    1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10) /*don’t delete it!*/
-	{
-		putchar(i); /*don’t delete it!*/
-	}
+	/* while (i < 10) */ 
+	/* { */
+		/* putchar(i); */ 
+	/* } */
 
 	printf("Infinite loop avoided! \\o/\n");
 
