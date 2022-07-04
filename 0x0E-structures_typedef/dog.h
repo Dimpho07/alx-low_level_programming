ff5607fc8478da53919ec265a5a7cf89c95b324d
Dimpho07 <dimplesaphiri@gmail.com>
2022-07-04 09:42:14 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,22 @@
-#ifndef _DOG.H_
-#define _DOG.H_
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * struct dog - Data type of a dog.
+ * @name: Name of Dog.
+ * @age: Age of Dog.
+ * @owner: Owner of Dog.
+ */
 struct dog
 {
-	char *name
-	float age
-	char *owner
+	char *name;
+	float age;
+	char  *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
 
 #endif
