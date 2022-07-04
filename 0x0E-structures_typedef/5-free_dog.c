9ca931303c925721febc8988bfa1df8c82f7e71f
Dimpho07 <dimplesaphiri@gmail.com>
2022-07-04 15:28:03 -0700

    4-new dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,13 +1,15 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * free_dog - Function that frees memory created in 4-new_dog.c
- * @d: Input struct
- * Return: Nothing
+ * free_dog - frees dogs.
+ * @d: struct dog.
+ *
+ * Return: no return.
  */
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
+	if (d)
 	{
 		free(d->name);
 		free(d->owner);
